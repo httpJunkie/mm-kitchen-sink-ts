616f93cd58c2344f8f7606fc37a0b4f45de0b4f1
Eric Bishard <e.bishard@gmail.com>
2022-06-26 02:52:28 -0700

    Add favicons for Chrome, iOS & Android, Safari, and pinnded tab

--- pages/layout/index.jsx
+++ pages/layout/index.jsx
@@ -1,8 +1,8 @@
 import { useContext } from 'react'
+import Head from 'next/head'
 import { ViewContext } from '../context/ViewContext'
 import { AppContext } from '../context/AppContext'
 import { useMediaPredicate } from 'react-media-hook'
-import useSWR from 'swr'
 
 import Logo from '../partial-components/Logo'
 import Sidenav from '../partial-components/Sidenav'
@@ -15,21 +15,33 @@ const Layout = ({ children }) => {
   const context = useContext(AppContext)
 
   return (
-    <div className={`app-container ${context.breakpoint} ${context.themeMode}`}>
-      <main>
-        <header>
-          <Logo />
-          <Topnav />
-        </header>
-        <section>
-          {children}
-        </section>
-        <footer>
-          <Foot />
-        </footer>
-      </main>
-      <Sidenav />
-    </div>
+    <>
+      <Head>
+        <title>MetaMask Kitchen Sink Dapp</title>
+        <meta name="description" content="MetaMask API Methods in Real World React Components" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <div className={`app-container ${context.breakpoint} ${context.themeMode}`}>
+        <main>
+          <header>
+            <Logo />
+            <Topnav />
+          </header>
+          <section>
+            {children}
+          </section>
+          <footer>
+            <Foot />
+          </footer>
+        </main>
+        <Sidenav />
+      </div>
+    </>
   )
 }
 
