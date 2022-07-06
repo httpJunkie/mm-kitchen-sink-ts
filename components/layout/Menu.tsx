f2bb8750622eed9eab6143d8c3d5e5b25b9d2172
Eric Bishard <e.bishard@gmail.com>
2022-07-06 08:35:54 -0700

    Allow RouterLinks component to take an array of AppRoute, and define ComponentProps for it.

--- components/layout/Menu.tsx
+++ components/layout/Menu.tsx
@@ -5,24 +5,26 @@ import { SiGithub } from 'react-icons/si'
 
 import Hamburger from '../ui/hamburger/Hamburger'
 
+interface AppRoute {
+  name: string;
+  href: string;
+}
+interface RouterLinksProps {
+  routes: AppRoute[]
+}
 
-const RouterLinks = (routes: any) =>
-  <>
-    {[
-      { name: "Home", href: "/" },
-      { name: "Signing", href: "/signing" },
-      { name: "Chain", href: "/chain" },
-      { name: "Accounts", href: "/accounts" },
-    ].map((route, idx) => {
-      return (
+const RouterLinks = ({ routes }: RouterLinksProps) => {
+  return <>
+    {routes.map(
+      (route, idx) =>
         <li className="link" key={`${route.name}-${idx}`}>
           <Link href={route.href}>
             <a className='text_link'>{route.name}</a>
           </Link>
         </li>
-      )
-    })}
+    )}
   </>
+}
 
 const Menu = () => {
   const context = useContext(AppContext)
@@ -30,10 +32,16 @@ const Menu = () => {
     context.toggleSidenav(!context.navOpen)
   }
 
+  const myAppRoutes: AppRoute[] = [
+    { name: "Home", href: "/" },
+    { name: "Signing", href: "/signing" },
+    { name: "Chain", href: "/chain" },
+    { name: "Accounts", href: "/accounts" }
+  ]
 
   return (
     <ul style={{ userSelect: 'none' }}>
-      <RouterLinks />
+      <RouterLinks routes={myAppRoutes} />
       <li className='link'>
         <a className='text_link' tabIndex={4} href='https://github.com/httpJunkie/mm-kitchen-sink-ts'>
           &nbsp;<SiGithub size="16" /> Source
